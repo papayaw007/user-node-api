0feb6735a9abded349e52b4a3270534454a3b5f0
papayaw007 <papa.adu-asare@ashesi.edu.gh>
2025-05-03 17:00:28 +0000

    First commit - basic REST API

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 import http from 'http';
 import { v4 as uuidv4 } from 'uuid';
 
-
 const PORT = 3000;
-
 const users = [];
 
 const server = http.createServer((req,res)=>{
@@ -14,18 +12,14 @@ const server = http.createServer((req,res)=>{
     }else if (req.url === '/users' && req.method === "POST"){
         let body = ''
         req.on('data', (chunk)=> body += chunk)
-        req.on('end', ()=>{
+        req.on('end', ()=> {
             const data = JSON.parse(body);
             const newUser = { id: uuidv4(), ...data};
             users.push(newUser);
             res.writeHead(200, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify(newUser));
+            res.end(JSON.stringify(users));
 
         })
-    }
-
-    
+    }   
 })
-
-
 server.listen(PORT, ()=> console.log(`server is runnning on port: ${PORT}`));
